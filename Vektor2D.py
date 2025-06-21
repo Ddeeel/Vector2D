5fb03106f6f414989168fa83f7d5e6e34126fe38
Ddeel <goncharkristina3557@gmail.com>
2025-06-21 13:28:23 +0300

    додавання методів

--- Vektor2D.py
+++ Vektor2D.py
@@ -12,3 +12,15 @@ class Vector2D:
         return self.x == other.x and self.y == other.y
     def str(self):
         return f"({self.x}, {self.y})"
+
+    def length(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def normalize(self):
+        length = self.length()
+        if length == 0:
+            return Vector2D(0, 0)
+        return Vector2D(self.x / length, self.y / length)
+
+    def dot_product(self, other):
+        return self.x * other.x + self.y * other.y
